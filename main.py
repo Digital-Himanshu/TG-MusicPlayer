32e2701d8541efb35a28bee711be7884a8d1bc5d
Mᴏᴏɴʟɪɢʜᴛ <87492539+Lost-In-Dark@users.noreply.github.com>
2021-10-09 13:45:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import os
-import asyncio
 from pyrogram import Client, idle
 from pytgcalls import PyTgCalls
 from pytgcalls import idle as pyidle
 from config import bot, call_py
 
-async def vcclient():
-   await bot.start()
-   print("UserBot Started")
-   await call_py.start()
-   print("Vc Client Started")
-   await pyidle()
-   await idle()
-
-asyncio.get_event_loop().run_until_complete(vcclient())
+bot.start()
+print("UserBot Started")
+call_py.start()
+print("Vc Client Started")
+pyidle()
+idle()
